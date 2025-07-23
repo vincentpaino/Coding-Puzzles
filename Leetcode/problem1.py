9eb73ad5126a1378d64ad4b9b5ec0024ad3c660f
Vincent Paino <vincentpaino11@gmail.com>
2025-07-23 00:48:26 -0500

    worked on roman numeral

--- Leetcode/problem1.py
+++ Leetcode/problem1.py
@@ -8,9 +8,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # Create a dictionary of key/value pairs of roman numeral strings/values.
+        roman_map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        for i in range(len(s)):
+            char = s[i]
+            if roman_map[char]:
+                
+
+
+
 
 
 
 if __name__ == "__main__":
-    result = solution()
+    result = Solution.romanToInt(self, "IV")
     print(f"The solution is: {result}")

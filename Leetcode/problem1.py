83e19b33cb65d216d6bbced15893ee8d58a672ba
Vincent Paino <vincentpaino11@gmail.com>
2025-07-22 23:01:56 -0500

    first commit

---
+++ Leetcode/problem1.py
@@ -0,0 +1,16 @@
+# Navigate to miniconda environment: conda activate Coding-Puzzles
+
+import math
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+
+if __name__ == "__main__":
+    result = solution()
+    print(f"The solution is: {result}")

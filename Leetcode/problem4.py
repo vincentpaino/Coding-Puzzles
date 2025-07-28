1f491c682ad36e8da38ae5099ced504f961743f6
Vincent Paino <vincentpaino11@gmail.com>
2025-07-28 00:27:59 -0500

    started problem 4

---
+++ Leetcode/problem4.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+    
+        return head
+
+if __name__ == "__main__":
+    print(Solution().removeNthFromEnd(head=[1,2,3,4,5], n=2))
